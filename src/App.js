05e09d23ea2a66ad5ef68f255a02d5b62f5ab4a5
subir-raju <subirdey@Subirs-MacBook-Pro.local>
2021-07-13 01:48:26 +0600

    Navbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router>
+       <Navbar />
+          <Switch>
+            <Route path='/' exact />
+          </Switch>
+    </Router>
+   
+    
+    </>
+  );
+}
+
+export default App;
